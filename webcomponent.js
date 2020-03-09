77039f25f7c0be5f4855ffede129129e661d7145
ivangarciatorrano <39955485+ivangarciatorrano@users.noreply.github.com>
2020-03-10 00:01:45 +0100

    Add files via upload

--- webcomponent.js
+++ webcomponent.js
@@ -1,9 +1,8 @@
 (function()  {
     let tmpl = document.createElement('template');
     tmpl.innerHTML = `
-        <h1>Hello ivan</h1>
+        <h1>Hello ivanWorld</h1>
     `;
-  
 
     customElements.define('com-sap-sample-helloworld1', class HelloWorld1 extends HTMLElement {
 
